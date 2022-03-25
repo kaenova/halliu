6d17590099277c490f3b372309535f6b97bc8b6e
Kaenova Mahendra Auditama <kaenova@gmail.com>
2022-03-25 16:21:26 +0700

    Connect Bantuan Form with API

--- frontend/components/ListBantuan/CardPertanyaan.jsx
+++ frontend/components/ListBantuan/CardPertanyaan.jsx
@@ -7,7 +7,7 @@ function CardPertanyaan(data) {
       <div className="card-body">
         <h2 className="card-title">{data.data["User"].name}</h2>
         <p>{`Pertanyaan : ${data.data.message}`}</p>
-        <p>{`Jawaban    : ${data.data.reply}`}</p>
+        <p>Jawaban : {data.data.reply? data.data.reply : "Masih belum dibalas"}</p>
         <div className='flex flex-row gap-3'>
           {data.data.image &&
             <img width={300} src={process.env.NEXT_PUBLIC_BACKEND + "/static" + data.data.image} alt="" />
--- frontend/pages/bantuan-form.jsx
+++ frontend/pages/bantuan-form.jsx
@@ -1,14 +1,57 @@
 import PageContainer from "../components/PageContainer"
 import DragDrop from "../components/DragDrop"
 import { FileUploader } from "react-drag-drop-files";
-import {useState} from 'react'
+import { useState, useEffect } from 'react'
+import { authApi } from "../utils/api";
 
 const BantuanForm = () => {
-  const [file, setFile] = useState(null);
+  const [FormInput, setFormInput] = useState({
+    message: "",
+    video: null,
+    image: null,
+  })
+
+  const [PesanBox, setPesanBox] = useState("")
+
   const handleChange = (file) => {
-    setFile(file);
+    console.log(file)
+    if (file.type == "video/mp4") {
+      setFormInput({ ...FormInput, video: file })
+    }
+    if (file.type == "image/jpeg") {
+      setFormInput({ ...FormInput, image: file })
+    }
   };
   const fileTypes = ["JPG", "MP4"];
+
+  const handleSubmitButton = (e) => {
+    e.preventDefault()
+    if (FormInput.message == "") {
+      setPesanBox("Pertanyaann tidak boleh kosong")
+      return
+    }
+
+    let formData = new FormData();
+    formData.append("message", FormInput.message)
+    formData.append("image", FormInput.image)
+    formData.append("video", FormInput.video)
+
+    authApi().post("/api/support", formData)
+      .then(() => {
+        setPesanBox("Berhasil memasukkan data, akan segera dibalas")
+        setTimeout(() => {
+          window.location.replace("/list-bantuan");
+        }, 3000);
+      })
+      .catch((e) => {
+        setPesanBox("Gagal dalam mengirimkan data, harap coba lagi")
+        setTimeout(() => {
+          window.location.replace("/bantuan");
+        }, 3000);
+      })
+  }
+
+
   return (
     <>
       <PageContainer>
@@ -23,21 +66,27 @@ const BantuanForm = () => {
               <div className="md:w-1/2 mx-auto">
                 <form action="">
                   <label className="label">
-                    <span className="label-text">Pertanyaan</span>
+                    <span className="label-text">Pertanyaan*</span>
                   </label>
-                  <textarea type="text" placeholder="Masukkan Pertanyaan Anda" className="input input-bordered w-full min-h-[250px] " />
+                  <textarea onChange={(e) => { setFormInput({ ...FormInput, message: e.target.value }) }} type="text" placeholder="Masukkan Pertanyaan Anda" className="input input-bordered w-full min-h-[250px] " />
 
                   <label className="label">
                     <span className="label-text">Bukti (JPEG/MP4)</span>
                   </label>
                   <FileUploader handleChange={handleChange} name="file" types={fileTypes}>
-                    <button className="h-[200px] border-2 w-[100%] rounded-md border-dashed opacity-60">
-                      Upload Disini
+                    <button onClick={(e) => e.preventDefault()} className="h-[200px] border-2 w-[100%] rounded-md border-dashed opacity-60">
+                      <p>Upload Disini</p>
+                      {FormInput.image != null && <p>Foto Terupload</p>}
+                      {FormInput.video != null && <p>Video Terupload</p>}
                     </button>
                   </FileUploader>
-
+                  {PesanBox !== "" &&
+                    <div className="border-2 border- w-full h-[50px] mt-4 rounded-md border-red-300 flex justify-center items-center">
+                      {PesanBox}
+                    </div>
+                  }
                   <div className="text-center mt-10 mb-4">
-                    <button className="btn btn-outline">Submit</button>
+                    <button onClick={handleSubmitButton} className="btn btn-outline">Submit</button>
                   </div>
                 </form>
               </div>
@@ -49,19 +98,19 @@ const BantuanForm = () => {
   )
 }
 
-// export async function getServerSideProps({ req, res }) {
-//   if (!req.cookies.auth) {
-//     return {
-//       redirect : {
-//         destination: "/masuk",
-//         permanent: false
-//       }
-//     }
-//   }
-//   return {
-//     props: {}
-//   }
-// }
+export async function getServerSideProps({ req, res }) {
+  if (!req.cookies.auth) {
+    return {
+      redirect: {
+        destination: "/masuk",
+        permanent: false
+      }
+    }
+  }
+  return {
+    props: {}
+  }
+}
 
 
 
